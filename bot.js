f1592cbe51c8393ece2818da810bd04f9c778451
Mohamedtounisi <53474089+Mohamedtounisi@users.noreply.github.com>
2019-08-23 12:53:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(client.user.tag + " Ready!");
-  const channel = client.channels.get("529747122552307742");
+  const channel = client.channels.get("612692760792924183");
   channel.join().then(connection => {
     console.log("Successfully connected.");
   }).catch(e => {
